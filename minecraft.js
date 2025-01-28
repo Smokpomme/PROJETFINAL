6a29203bc091eeea4f5ca592e6ff34bf0300a0e9
Blary Erwan <erwanavril0204@gmail.com>
2025-01-28 21:53:22 +0100

    type="module"
    type="module"

--- minecraft.js
+++ minecraft.js
@@ -1,29 +1,4 @@
-var countDownDate = new Date("Dec 31, 2024 23:59:59").getTime();
-
-var x = setInterval(function() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("days").innerHTML = days;
-  document.getElementById("hours").innerHTML = hours;
-  document.getElementById("minutes").innerHTML = minutes;
-  document.getElementById("seconds").innerHTML = seconds;
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdown").innerHTML = "EXPIRED";
-  }
-}, 1000);
-
-
-
-
-    window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function() {
         const header = document.querySelector('.header');
         const footer = document.querySelector('.footer');
         const footerRect = footer.getBoundingClientRect();
